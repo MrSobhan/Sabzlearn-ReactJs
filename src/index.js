038fda48dcbb9c400a694d042bf8e154da09d6bd
MrSobhan <sobhan.mosazadeh85@gmail.com>
2024-04-22 21:47:54 +0330

    Arrange project structure

--- src/index.js
+++ src/index.js
@@ -2,13 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 
-import App from "./App";
+import App from "./frontend/App";
 
-import './styles/reset.css'
-import './styles/fonts.css'
-import './styles/variables.css'
-import './styles/helpers.css'
-import './styles/defaults.css'
+import './frontend/styles/reset.css'
+import './frontend/styles/fonts.css'
+import './frontend/styles/variables.css'
+import './frontend/styles/helpers.css'
+import './frontend/styles/defaults.css'
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
