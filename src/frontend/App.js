544857391c4dd563de25679d234086a09950f9f8
MrSobhan <sobhan.mosazadeh85@gmail.com>
2024-05-07 22:45:11 +0330

    Add BackEnd Project & Ui Login , Register

--- src/frontend/App.js
+++ src/frontend/App.js
@@ -2,6 +2,8 @@ import React from 'react'
 import { useRoutes } from 'react-router-dom'
 import routes from './routes'
 
+// import './App.css'
+
 export default function App() {
 
   const router = useRoutes(routes)
